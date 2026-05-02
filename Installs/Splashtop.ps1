e3c628b769bdb061d60f736e19eef98afff4f57c
MM <10528769+discosoc@users.noreply.github.com>
2026-05-02 12:56:41 -0800

    Update Splashtop.ps1

--- Installs/Splashtop.ps1
+++ Installs/Splashtop.ps1
@@ -65,6 +65,7 @@ Write-Host "Extraction complete."
 Write-Host "Installing Splashtop Streamer..."
 Write-Host $splashtopcode
 Start-Process -Wait -FilePath "msiexec.exe" -ArgumentList "/i `"$installFile`" /quiet /norestart USERINFO=`"decode=$splashtopcode,hidewindow=1,confirm_d=0`""
+Write-Host "/i `"$installFile`" /quiet /norestart USERINFO=`"decode=$splashtopcode,hidewindow=1,confirm_d=0`""
 Write-Host "Splashtop Streamer installed successfully."
 
 
