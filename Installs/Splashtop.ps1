3636c15e088ac8f341bc7ec70ff64d5a5c72e917
MM <10528769+discosoc@users.noreply.github.com>
2026-05-02 12:44:48 -0800

    Update Splashtop.ps1

--- Installs/Splashtop.ps1
+++ Installs/Splashtop.ps1
@@ -18,7 +18,7 @@ if (-not $splashtopcode) {
 # --- Variables ---
 $ProgressPreference = 'SilentlyContinue'
 $workingDir    = "C:\Scripts"
-$downloadUri   = "https://my.splashtop.com/team_deployment/download_directly/msi/5A7PPZR5J3JT"
+$downloadUri   = "https://my.splashtop.com/team_deployment/download_directly/msi/".$splashtopcode
 $outFile       = "$workingDir\SplashtopStreamer.zip"
 $installFile   = "$workingDir\SplashtopStreamer.msi"
 $detectionName = "*Splashtop Streamer*"
